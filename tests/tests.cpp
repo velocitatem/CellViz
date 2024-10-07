4164ab74167f36f1b9f3211425693feb91b3e51b
Daniel Rosel <daniel@alves.world>
2024-10-07 16:52:37 +0200

    testing added

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,3 +3,16 @@
 //
 
 
+#include <gtest/gtest.h>
+
+/// tes t1 + 1
+///
+
+TEST(test1, test1) {
+    EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
