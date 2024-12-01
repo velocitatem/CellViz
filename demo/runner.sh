81d2778a4bdf5b575aa554717e270d8dc85c29cf
Daniel Rosel <daniel@alves.world>
2024-12-01 17:17:33 +0100

    addedd running script

---
+++ demo/runner.sh
@@ -0,0 +1,10 @@
+BINARIES="/home/velocitatem/CLionProjects/CellViz/cmake-build-debug/"
+STOCKS_LIST=(APPL GOOGL MSFT TSLA AMZN)
+for STOCK in "${STOCKS_LIST[@]}"
+do
+  echo "Processing $STOCK"
+  $BINARIES/data $STOCK
+  $BINARIES/CellViz output.json $STOCK".mp4"
+  rm -f frame*.png
+done
+rm -f output.json
